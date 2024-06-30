46c838fe96346532666ac01ffa1fe88cfb0b9fd3
Ramil <undassynovramil@gmail.com>
2024-06-30 15:13:19 +0500

    1.tex -> 18.tex first pass done

--- src/.auctex-auto/main.el
+++ src/.auctex-auto/main.el
@@ -1,19 +1,36 @@
-;; -*- lexical-binding: t; -*-
-
 (TeX-add-style-hook
  "main"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "src/packages"
+    "src/first_pages"
+    "src/ict/1"
+    "src/ict/2"
     "src/ict/3"
+    "src/ict/4"
+    "src/ict/5"
+    "src/ict/6"
+    "src/ict/7"
+    "src/ict/8"
+    "src/ict/9"
+    "src/ict/10"
+    "src/ict/11"
+    "src/ict/12"
+    "src/ict/13"
+    "src/ict/14"
+    "src/ict/15"
+    "src/ict/16"
+    "src/ict/17"
+    "src/ict/18"
+    "src/last_page"
     "book"
     "bk10"))
  :latex)
--- src/ict/.auctex-auto/11.el
+++ src/ict/.auctex-auto/11.el
@@ -1,7 +1,5 @@
-;; -*- lexical-binding: t; -*-
-
 (TeX-add-style-hook
- "12"
+ "11"
  (lambda ()
    (LaTeX-add-labels
     "kix.87gpl39zxgv2"))
