3011c302e1bcd25b6be1fb13384e5d0e32f6310f
Ramil <undassynovramil@gmail.com>
2024-07-04 00:04:50 +0500

    fuck chem done

--- src/.auctex-auto/main.el
+++ src/.auctex-auto/main.el
@@ -1,19 +1,17 @@
-;; -*- lexical-binding: t; -*-
-
 (TeX-add-style-hook
  "main"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "src/packages"
-    "src/chem/13"
+    "src/chem/12"
     "book"
     "bk10"))
  :latex)
