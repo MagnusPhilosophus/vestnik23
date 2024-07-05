41ad2ad5c09273a7d8da0a81d1d3650dfb5352d7
Ramil <ramilendo200320@gmail.com>
2024-07-05 12:31:49 +0500

    gorn done, now dois

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ PDF_FILE := $(BUILD_DIR)/$(PDF_TARGET).pdf
 pdf: $(PDF_FILE)
 
 $(PDF_FILE): src/main.tex
-	latexmk -xelatex -pvc -output-directory=$(BUILD_DIR) -jobname=$(PDF_TARGET) src/main.tex
+	latexmk -f -xelatex -pvc -output-directory=$(BUILD_DIR) -jobname=$(PDF_TARGET) src/main.tex
 clean:
 	latexmk -C src/main.tex
 	rm -rf $(BUILD_DIR)/*
--- src/.auctex-auto/packages.el
+++ src/.auctex-auto/packages.el
@@ -5,12 +5,12 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("hyperref" "hidelinks") ("geometry" "margin=3cm") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "fontspec"
