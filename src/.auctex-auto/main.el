61ff031c58d57b3bcd57ccbe02ba7ae56798fd86
Ramil <undassynovramil@gmail.com>
2024-07-06 22:18:29 +0500

    ict envelopes

--- src/.auctex-auto/main.el
+++ src/.auctex-auto/main.el
@@ -11,7 +11,21 @@
    (TeX-run-style-hooks
     "latex2e"
     "src/packages"
-    "src/ict/1"
+    "src/chem/1"
+    "src/chem/2"
+    "src/chem/3"
+    "src/chem/4"
+    "src/chem/5"
+    "src/chem/6"
+    "src/chem/7"
+    "src/chem/8"
+    "src/chem/9"
+    "src/chem/10"
+    "src/chem/11"
+    "src/chem/12"
+    "src/chem/13"
+    "src/chem/14"
+    "src/chem/15"
     "book"
     "bk10"))
  :latex)
